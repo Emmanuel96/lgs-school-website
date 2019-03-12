537f011c63e0583612864bff4f16edf331ecc3ad
Elliot Ejeta <elliototuoniyo@gmail.com>
2019-03-12 20:17:58 +0100

    second push

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,10 +10,26 @@ class HomeController extends Controller
 {
     public function home()
     {
+        $campus_data = DB::table('addo')->selectRaw('*')->get();
+        //return $campus_data[0]->campus_name1;
+
+        
+        $badore_data = DB::table('badore')->selectRaw('*')->get();
+       // return $badore_data[0]->campus_name2;
+        
         $header_data = DB::table('header')->selectRaw('*')->get();
         // return $header_data[0]->welcome_text; 
 
-        return view('home', ['welcome_text'=> $header_data[0]->welcome_text, 'intro_text' => $header_data[0]->intro_text, 'button_text' => $header_data[0]->button_text]);
+        return view('home', ['welcome_text'=> $header_data[0]->welcome_text, 
+        
+        'intro_text' => $header_data[0]->intro_text, 
+        'button_text' => $header_data[0]->button_text,
+        'campus_name1' =>  $campus_data[0]->campus_name1,
+        'campus_name2'=> $badore_data[0]->campus_name2,
+        'campus_description1' => $campus_data[0]->campus_description1, 
+         'campus_image1' => $campus_data[0]->campus_image1,
+         'campus_description2' => $badore_data[0]->campus_description2, 
+         'campus_image2' => $badore_data[0]->campus_image2]);
     }
 
 
