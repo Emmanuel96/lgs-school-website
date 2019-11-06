5a4d4d27fca93798526da477786379ff518cc5a6
Emmanuel Audu <eaudu@gidimobile.com>
2019-11-06 10:57:43 +0100

    changes to the campus section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -145,7 +145,7 @@
         <div class="row">
           <div class="col-lg-12 text-center">
             <h2 class="section-heading text-uppercase">Campuses</h2>
-            <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+            <h3 class="section-subheading text-muted">Our Campuses are built to teach.</h3>
           </div>
         </div>
         <div class="row text-center">
