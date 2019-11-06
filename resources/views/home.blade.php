bc1dcf56fc45987ac8087f3d04305a549dab68f1
Emmanuel Audu <eaudu@gidimobile.com>
2019-11-06 10:38:17 +0100

    changed to ico logo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -74,7 +74,7 @@
     <nav style="background-color: black;" class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
       <div class="container">
         <a class="navbar-brand js-scroll-trigger" href="#page-top">
-          <img style = "width: 50px; height: 50px;" src = "{{'images/logo.png'}}" />
+          <img style = "width: 50px; height: 50px;" src = "{{'images/logo.ico'}}" />
           Lords Gate Montessori
         </a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
